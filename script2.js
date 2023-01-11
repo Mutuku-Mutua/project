94c049973590b58b1fa92e97137adcf1356ea0e4
Mutuku Mutua <mutua.mutuku@student.moringaschool.com>
2023-01-11 14:37:03 +0300

    Commits

--- script2.js
+++ script2.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
     
-    if (username === "user" && password === "web_dev") {
+    if (username === "mutua" && password === "success") {
         // alert("You have successfully logged in.");
         loginButton.addEventListener('click',()=>{
             createMainContent()  

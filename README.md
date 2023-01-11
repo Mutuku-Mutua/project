3c7f6ff6cb704640607a340ddf7697ea8ff1a4d2
Mutuku Mutua <mutua.mutuku@student.moringaschool.com>
2023-01-11 10:26:44 +0300

    commit changes

---
+++ README.md
@@ -0,0 +1,13 @@
+##PROJECT NAME Weather App.
+
+##Project-Description this application is for checking the weather status of various cities in the world
+
+##Instructions Simple weather app that displays the current day, month and year,also displays the temperature of the city in Fahrenheit(°F) , on click on the temperature section the user can convert the temperature from fahrenheit to celsius and also the Highs and Lows of the city in fahrenheit .
+
+##OPerating On the search bar the user will enter a city of they choice and press the enter key for it to run.
+
+##Technologies this app was build on pure HTML and javascript and style with css.
+
+##License This project is licensed under MIT license.
+
+##author Mutuku Mutua
--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-function print(value){
-    console.log(value)
-}
 
- const  loginButton = document.getElementById("login-btn")
-// print (loginButton)
+ const searchBox = document.querySelector('.search-box');
+ searchBox.addEventListener('keypress',setQuery);
+ const backgroundImg = document.querySelector("img")
+ function setQuery(event) {
+    if (event.keyCode == 13){
+        getResults(searchBox.value);
+        searchBox.value = ""   
+    } 
+ }
+
+ function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&unit=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+
+ }
+
+ function displayResults (weather) {
+    console.log(weather);
+    let city = document.querySelector('.Location .City');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.Location .date');
+    date.innerText = dateBuilder(now);
+
+
+     let temp = document.querySelector('.Temp');
+     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F </span>`;
+
+     let celsius = (weather.main.temp - 32) * (5 / 9);
+
+     temp.addEventListener("click", () =>{
+      temp.innerHTML = `${Math.floor(celsius)}<span>°C</span>`
+      
+     })
+
+     let weatherEl = document.querySelector('.current .weather');
+     weatherEl.innerText = weather.weather[0].main;
+
+     
+
+     let hiLow = document.querySelector('.hi-low');
+     hiLow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+
+     backgroundImg.src = "https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+
+ }    
+ function dateBuilder (d) {
+    let months = ["January","February","March","April","May",
+    "June","July","August","september",
+    "October","November","December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday",
+    "Thursday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+ }
